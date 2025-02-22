e44371af641e77936c512dc553a3cf60c8119bed
hvenanc <hvs.git@gmail.com>
2025-02-22 20:23:41 -0300

    Registro Básico de Frequência no Sistema

---
+++ frequencia/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from frequencia.views import *
+
+app_name = 'frequencia'
+
+urlpatterns = [
+    path('<int:turma_id>/', turma_detalhes, name='frequencia'),
+]
--- sistemaescolar/urls.py
+++ sistemaescolar/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('alunos/', include('alunos.urls')),
-    path('turmas/', include('turmas.urls'))
+    path('turmas/', include('turmas.urls')),
+    path('frequencia/', include('frequencia.urls')),
 ]
